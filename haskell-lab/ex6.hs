a6b81ca113d88b13e3d4e9db3d0f09ec2b29656f
SMAntony <64364731+SMAntony@users.noreply.github.com>
2021-09-15 12:57:38 +0530

    Update ex6.hs

--- haskell-lab/ex6.hs
+++ haskell-lab/ex6.hs
@@ -1,10 +1,12 @@
 import Data.List (permutations, minimum, delete)
 
+--Selection Sort
 ssort :: Ord a => [a] -> [a]
 ssort [] = []
 ssort xs = let { x = minimum xs } 
            in  x : ssort (delete x xs)
 
+--Insertion Sort
 isort :: Ord a => [a] -> [a]
 isort [] = []
 isort (x:xs) = insert x $ isort xs
@@ -14,6 +16,7 @@ isort (x:xs) = insert x $ isort xs
       |x<=y = x:y:ys
       |otherwise = y: insert x ys
 
+--Bubble Sort
 bsort :: Ord a => [a] -> [a]
 bsort s = case bsort' s of
                t | t == s    -> t
@@ -22,21 +25,20 @@ bsort s = case bsort' s of
                          | otherwise = x:(bsort' (x2:xs))
         bsort' s = s
 
-
-sorted :: Ord a => [a] -> Bool
+--Permutation Sort
+sorted :: Ord a => [a] -> Bool    --check sort
 sorted (x:y:xs) = x <= y && sorted (y:xs)
 sorted _        = True
-psort :: Ord a => [a] -> [a]
+psort :: Ord a => [a] -> [a]     --sort function
 psort = head . filter sorted . permutations
 
-
-
-merge :: Ord a => [a] -> [a] -> [a]
+--Merge Sort
+merge :: Ord a => [a] -> [a] -> [a] --Merge Function
 merge xs [] = xs
 merge [] ys = ys
 merge (x:xs) (y:ys) | x <= y    = x:merge xs (y:ys)
                     | otherwise = y:merge (x:xs) ys
-msort :: Ord a => [a] -> [a]
+msort :: Ord a => [a] -> [a] --Sort Function
 msort [] = []
 msort [a] = [a]
 msort xs = merge (msort (firstHalf xs)) (msort (secondHalf xs))
@@ -44,6 +46,7 @@ msort xs = merge (msort (firstHalf xs)) (msort (secondHalf xs))
 firstHalf  xs = let { n = length xs } in take (div n 2) xs
 secondHalf xs = let { n = length xs } in drop (div n 2) xs
 
+--main
 main :: IO()
 main  = do
     let lists = [13, 23, 22, 454]
