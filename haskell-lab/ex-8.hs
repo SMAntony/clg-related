62ec067064eebb8481a84873d4d5bf3959869566
SMAntony <64364731+SMAntony@users.noreply.github.com>
2021-09-15 20:42:06 +0530

    Add files via upload

---
+++ haskell-lab/ex-8.hs
@@ -0,0 +1,20 @@
+import Data.Char ( toUpper )
+import System.IO ( IOMode(ReadMode), hGetContents, openFile )
+
+readAndWrite :: FilePath -> FilePath -> IO ()
+readAndWrite fileName newFileName= do
+  handle <- openFile fileName ReadMode
+  contents <- hGetContents handle
+  putStrLn "The contents in the file are:\n"
+  putStrLn contents
+  let convertedText = map toUpper contents
+  writeFile newFileName convertedText
+  putStrLn "\nFile Written."
+  putStrLn "The contents of the new file are:\n"
+  putStrLn convertedText
+
+main :: IO ()
+main = do
+  putStr "Enter the file path:"
+  fileName <- getLine
+  readAndWrite fileName (fileName++"_converted.txt")
