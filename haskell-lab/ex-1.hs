fba3d584f922349b162b67e920f13cdaa91f6e7e
SMAntony <64364731+SMAntony@users.noreply.github.com>
2021-09-15 23:47:22 +0530

    Update ex-1.hs

--- haskell-lab/ex-1.hs
+++ haskell-lab/ex-1.hs
@@ -1,5 +1,14 @@
 maxOfThree :: Int -> Int -> Int -> Int
-maxOfThree a b c
+maxOfThree a b c =
+  if a>c && a>b
+  then a
+  else
+    if b>c
+    then b
+    else c
+
+maxOfThree' :: Int -> Int -> Int -> Int
+maxOfThree' a b c
     | a>c && a>b = a
     | b>c = b
     | otherwise = c
